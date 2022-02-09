03e1e1c5bb68c89ceeeb84980ee249f254d77ad8
o.borzykin <borzykin3455@gmail.com>
2022-02-09 20:14:17 +0200

    Added allure logging

--- src/test/java/services/BaseService.java
+++ src/test/java/services/BaseService.java
@@ -1,33 +1,24 @@
 package services;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
-import io.restassured.config.ObjectMapperConfig;
-import io.restassured.config.RestAssuredConfig;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
-import io.restassured.mapper.ObjectMapperType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 import static helpers.EnvConfig.ENV_CONFIG;
 
 public class BaseService {
-    protected final RequestSpecification requestSpec;
-    protected final ResponseSpecification responseSpec;
-
-    public BaseService() {
-        var config = RestAssuredConfig.config()
-                .objectMapperConfig(new ObjectMapperConfig(ObjectMapperType.GSON));
-        this.requestSpec = new RequestSpecBuilder()
-                .setConfig(config)
-                .setBaseUri(ENV_CONFIG.getApiUrl())
-                .setContentType(ContentType.JSON)
-                .setAccept(ContentType.JSON)
-                .log(LogDetail.ALL)
-                .build();
-        this.responseSpec = new ResponseSpecBuilder()
-                .log(LogDetail.ALL)
-                .build();
-    }
+    protected static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .addFilter(new AllureRestAssured())
+            .setBaseUri(ENV_CONFIG.getApiUrl())
+            .setContentType(ContentType.JSON)
+            .setAccept(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    protected static final ResponseSpecification responseSpec = new ResponseSpecBuilder()
+            .log(LogDetail.ALL)
+            .build();
 }
